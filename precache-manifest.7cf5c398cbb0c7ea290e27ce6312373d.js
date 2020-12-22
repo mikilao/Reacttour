04f79a39ed5628749c05f214cd669293fece521a
Rachel <thelivedish@gmail.com>
2020-12-22 09:16:07 -0800

    Updates

---
+++ precache-manifest.7cf5c398cbb0c7ea290e27ce6312373d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a0c2ed57b8b04d099bf15ca831adeef",
+    "url": "/Reacttour/index.html"
+  },
+  {
+    "revision": "f51d51cb0ff470bf6613",
+    "url": "/Reacttour/static/css/main.8d0f8ca5.chunk.css"
+  },
+  {
+    "revision": "ca208eca5043552739bd",
+    "url": "/Reacttour/static/js/2.abe25f63.chunk.js"
+  },
+  {
+    "revision": "cb4293a028d0175415ddb31dc1aaaefb",
+    "url": "/Reacttour/static/js/2.abe25f63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f51d51cb0ff470bf6613",
+    "url": "/Reacttour/static/js/main.83b0a6bd.chunk.js"
+  },
+  {
+    "revision": "e9481513be64e7f37b4b",
+    "url": "/Reacttour/static/js/runtime-main.7a7fa79e.js"
+  },
+  {
+    "revision": "3d347ff93c97c5c618a58c7d77e7363d",
+    "url": "/Reacttour/static/media/logo.3d347ff9.jpg"
+  }
+]);
